9eecd785b3f3eb5afe3f4f3e0f4315159b6454dc
Alex Caiza <alex.raul.caiza.morillo@gmail.com>
2019-12-07 09:23:47 -0500

    config firebase

--- src/app/componentes/register-page/register-page.component.ts
+++ src/app/componentes/register-page/register-page.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 import { AuthService } from '../../servicios/auth.service';
-import {Router } from '@angular/router';
-import {FlashMessagesService} from 'angular2-flash-messages';
+import { Router } from '@angular/router';
+import { FlashMessagesService } from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-register-page',
@@ -13,6 +13,7 @@ export class RegisterPageComponent implements OnInit {
 
   public email: string;
   public password: string;
+
   constructor(
     public authService: AuthService,
     public router: Router,
@@ -22,15 +23,17 @@ export class RegisterPageComponent implements OnInit {
   ngOnInit() {
   }
   onSubmitAddUser() {
+    console.log('method onSubmitAddUser..');
     this.authService.registerUser(this.email, this.password)
-    .then((res) => {
-      this.flashMensaje.show('Usuario creado correctamente.',
-      {cssClass: 'alert-success', timeout: 4000});
-     this.router.navigate(['/privado']);
-    }).catch( (err) => {
-      this.flashMensaje.show(err.message,
-      {cssClass: 'alert-danger', timeout: 4000});
-    });
+      .then((res) => {
+        console.log('BIEN!!!!');
+        console.log(res);
+        this.flashMensaje.show('Usuario creado correctamente.', { cssClass: 'alert-success', timeout: 4000 });
+        this.router.navigate(['/privado']);
+      }).catch((err) => {
+        console.log(err);
+        this.flashMensaje.show(err.message, { cssClass: 'alert-danger', timeout: 4000 });
+      });
   }
 
 }
